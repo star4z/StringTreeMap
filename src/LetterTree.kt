483bd40d5b4af7ea8fc72b93d83644f65d6e843f
Benjamin Phillips <bdphillips013@gmail.com>
2019-03-02 22:21:37 -0500

    Used a different algorithm to get better results

--- src/LetterTree.kt
+++ src/LetterTree.kt
@@ -3,71 +3,117 @@ import java.util.*
 import kotlin.collections.HashMap
 
 fun main() {
-    val root = readDictionaryAsBinary()
-//        createLetterDictionary()
-//    writeDictionaryAsBinary(root)
+//    val root = readDictionaryAsBinary<HashMap<String, String>>("dictionary_compact_2.dat")
+//        val root = createDictionary()
+//    val root = createLetterDictionary()
+//    writeDictionaryAsBinary(root, "dictionary_compact_2.dat")
+
+    val root = createLetterDictionary()
+    writeDictionaryAsBinary(root, "dictionary_compact_3.dat")
+
 
     var cont = true
     var query: String?
     while (cont) {
         println("Enter a word to look up:")
         query = readLine()
-        println(root.search(query!!.toLowerCase()) ?: "Word does not exist.")
+        val row = query!!.length
+        val letter = query[query.lastIndex]
+        val value = valueOf(query.trim())
+        println(root[row][letter]?.get(value) ?: "Word does not exist.")
         println("Would you like to look up another word?")
         cont = readLine()!!.toLowerCase().contains("y")
     }
 }
 
-fun createLetterDictionary(): Letter<String> {
+fun createDictionary(): HashMap<String, String> {
     val file = File("dictionary_compact.json")
     val scanner = Scanner(file)
     var s = scanner.nextLine()
     scanner.close()
 
+    var savedChars = 0
+    var totalChars = 0
+
     s = s.substring(1, s.length - 1)
 
     val r = s.split("\",\"")
 
+    val dictionary = HashMap<String, String>()
+
     val root = Letter<String>()
     r.forEach {
         val t = it.split("\":\"")
-        var prev = root
-        t[0].forEachIndexed { index, c ->
-            val nextLetter = Letter<String>(c)
-            if (prev.nextLetters.contains(c)) {
-                prev = prev.nextLetters[c]!!
 
-            } else {
-                prev.nextLetters[c] = nextLetter
-                prev = nextLetter
-            }
-            if (index == t[0].lastIndex) {
-                prev.isWordEnding = true
-                prev.definition = t[1]
-            }
-        }
+        dictionary[t[0]] = t[1]
     }
-
-    return root
+    return dictionary
 }
 
-fun writeDictionaryAsBinary(root: Letter<String>) {
-    val fileOutputStream = FileOutputStream("dictionary_compact.dat")
+fun createLetterDictionary(): List<HashMap<Char, HashMap<Int, String>>> {
+    val file = File("dictionary_compact.json")
+    val scanner = Scanner(file)
+    var s = scanner.nextLine()
+    scanner.close()
+
+    var savedChars = 0
+    var totalChars = 0
+
+    s = s.substring(1, s.length - 1)
+
+    val list = List<HashMap<Char, HashMap<Int, String>>>(45) { HashMap(26) }
+    val r = s.split("\",\"")
+    r.forEach {
+        val t = it.split("\":\"")
+        val row = t[0].length
+        val letter = t[0][t[0].lastIndex]
+        val value = valueOf(t[0])
+        if (!list[row].contains(letter)) {
+            list[row][letter] = HashMap()
+        } else if (list[row][letter]!!.contains(value)) {
+            throw IllegalArgumentException("${t[0]}'s definition overlaps with ${list[row][letter]!![value]}: $value")
+        }
+        totalChars += t[0].length
+        savedChars += t[0].length - 1
+        list[row][letter]!![value] = t[1]
+
+    }
+
+    println("Congratulations! You saved $savedChars chars worth of data instead of $totalChars!")
+
+    return list
+}
+
+val powersOf26 = Array(45) { Math.pow(26.0, it.toDouble()) }
+
+fun valueOf(c: Char, pos: Int): Double = (c - 'a' + 1).toDouble() *
+        if (pos < 45) powersOf26[pos] else Math.pow(26.0, pos.toDouble())
+
+fun valueOf(string: String): Int {
+    return string.hashCode()
+//    var value = 0.0
+//    string.forEachIndexed { i, c ->
+//        value += valueOf(c, i)
+//    }
+//    return value
+}
+
+fun <T> writeDictionaryAsBinary(dictionary: T, fileName: String) {
+    val fileOutputStream = FileOutputStream(fileName)
     val objectOutputStream = ObjectOutputStream(fileOutputStream)
 
-    objectOutputStream.writeObject(root)
+    objectOutputStream.writeObject(dictionary)
 
     objectOutputStream.close()
 }
 
-fun readDictionaryAsBinary(): Letter<String> {
-    val fileInputStream = FileInputStream("dictionary_compact.dat")
+fun <T> readDictionaryAsBinary(fileName: String): T {
+    val fileInputStream = FileInputStream(fileName)
     val objectInputStream = ObjectInputStream(fileInputStream)
-
-    return objectInputStream.readObject() as Letter<String>
+    return objectInputStream.readObject() as T
 }
 
-class Letter <T> (
+class Letter<T>(
     val c: Char = '\u0000',
     val nextLetters: HashMap<Char, Letter<T>> = HashMap(),
     var isWordEnding: Boolean = false,
@@ -79,7 +125,7 @@ class Letter <T> (
         return "$nextLetters"
     }
 
-    fun search(query: String): T? {
+    fun get(query: String): T? {
 
         return if (query.isEmpty()) {
             if (isWordEnding) {
@@ -89,7 +135,7 @@ class Letter <T> (
             }
         } else {
             if (nextLetters.containsKey(query[0])) {
-                nextLetters[query[0]]!!.search(query.substring(1))
+                nextLetters[query[0]]!!.get(query.substring(1))
             } else {
                 null
             }
